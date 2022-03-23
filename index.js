6200ffd87badfffc4b06dfb784929af3f84067b5
UtsavAch <acharya.utsav.15@gmail.com>
2022-03-23 14:47:39 +0000

    Refactor some code

--- index.js
+++ index.js
@@ -2,24 +2,10 @@ const fs = require("fs");
 const http = require("http");
 const url = require("url");
 
+const replaceTemplate = require("./modules/replaceTemplate");
+
 ///////////////////////////////
 /////SERVER
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-
-  if (!product.organic)
-    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-  return output;
-};
-
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
   "utf-8"
@@ -64,7 +50,6 @@ const server = http.createServer((req, res) => {
   } else {
     res.writeHead(404, {
       "Content-type": "text/html",
-      "my-own-header": "hello-world",
     });
     res.end("<h1>Page not found!<h1>");
   }
